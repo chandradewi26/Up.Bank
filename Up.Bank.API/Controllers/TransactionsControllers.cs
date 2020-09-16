3fe83995a030e44e0b19de165a80f92d4c0b8ad0
chandradewi26 <chandradewi2691@gmail.com>
2020-09-16 22:42:40 +1000

    Adding Transaction Controllers

---
+++ Up.Bank.API/Controllers/TransactionsControllers.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Up.Bank.API.Controllers
+{
+    [Route ("up_banking/api/[controller]")]
+    //https://localhost:44331/up_banking/api/Transactions
+    // : = inherit
+    public class TransactionsController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetTransaction()
+            //Could be OK result or Error result - 200 - 400 - HTTP Status
+        {
+            return Ok("Success!!");
+        }
+
+
+
+
+    }
+    
+}
