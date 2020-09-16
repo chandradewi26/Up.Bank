5240d557d50bc324cdf721c89581573191d0eef4
chandradewi26 <chandradewi2691@gmail.com>
2020-09-16 22:24:21 +1000

    Add Business Logic / Domain Model / Transaction.cs
    Add Business Logic / Logic / TransactionLogic.cs

---
+++ Up.Bank.BusinessLogic/Logic/TransactionLogic.cs
@@ -0,0 +1,16 @@
+﻿
+using Up.Bank.BusinessLogic.DomainModel;
+
+namespace Up.Bank.BusinessLogic.Logic
+{
+    public class TransactionLogic
+    {
+        public Transaction GetNewTransaction()
+        {
+            var transaction2 = new Transaction("Butcher", 50);
+
+
+            return transaction2;
+        }
+    }
+}
