a879941884bb0e3e7ba7d41807456596225f445d
chandradewi26 <chandradewi2691@gmail.com>
2020-10-04 19:58:24 +1100

    Revising fields, constructor and method of Account.cs  according to API UPBank
    #There's error in DateTime
    
    Editing AccountLogic.cs
    #Still not sure at what logic should i prep.

--- Up.Bank.BusinessLogic/DomainModel/Account.cs
+++ Up.Bank.BusinessLogic/DomainModel/Account.cs
@@ -10,25 +10,126 @@ namespace Up.Bank.BusinessLogic.DomainModel
 
         public Account()
         {
-            accountID = 0000000000;
-            firstName = "";
-            lastName = "";
-            address = "";
+            DisplayName = "No Account were stored yet"; // Account Name - Savings, Tuition Fee, etc.
+            Value = 0;
+            AccountType = "Saver / Transactional";
         }
 
-        public Account(int ID, string fname, string lname, string address)
+        public Account(decimal value, string name, string type)
         {
-            if (ID < 0 || ID > 9999999999 )
-                throw new Exception("Account ID invalid");
+            if (value < 0)
+                throw new Exception("Value invalid, value cant be lower than 0");
 
-            accountID = ID;
-            firstName = "";
-            lastName = "";
-            address = "";
+            DisplayName = name;
+            Value = value;
+            AccountType = type;
         }
-        public string firstName { get; set; }
-        public string lastName { get; set; }
-        public string address { get; set; }
-        public decimal accountID { get; set; }
+        public string DisplayName { get; set; }
+        public string AccountType { get; set; }
+        public decimal Value { get; set; }
     }
 }
+
+
+
+
+/*
+ * 
+    "data": [
+        {
+            "type": "accounts",
+            "id": "4e7f1557-92ca-458f-8682-f7902ec1bb91",
+            "attributes": {
+                "displayName": "Up Account",
+                "accountType": "TRANSACTIONAL",
+                "balance": {
+                    "currencyCode": "AUD",
+                    "value": "90.46",
+                    "valueInBaseUnits": 9046
+                },
+                "createdAt": "2020-02-17T13:44:51+11:00"
+            },
+            "relationships": {
+                "transactions": {
+                    "links": {
+                        "related": "https://api.up.com.au/api/v1/accounts/4e7f1557-92ca-458f-8682-f7902ec1bb91/transactions"
+                    }
+                }
+            },
+            "links": {
+                "self": "https://api.up.com.au/api/v1/accounts/4e7f1557-92ca-458f-8682-f7902ec1bb91"
+            }
+        },
+        {
+            "type": "accounts",
+            "id": "7131df79-a395-4eaf-b954-8d026086915a",
+            "attributes": {
+                "displayName": "Savings",
+                "accountType": "SAVER",
+                "balance": {
+                    "currencyCode": "AUD",
+                    "value": "3705.40",
+                    "valueInBaseUnits": 370540
+                },
+                "createdAt": "2020-02-17T23:59:47+11:00"
+            },
+            "relationships": {
+                "transactions": {
+                    "links": {
+                        "related": "https://api.up.com.au/api/v1/accounts/7131df79-a395-4eaf-b954-8d026086915a/transactions"
+                    }
+                }
+            },
+            "links": {
+                "self": "https://api.up.com.au/api/v1/accounts/7131df79-a395-4eaf-b954-8d026086915a"
+            }
+        },
+        {
+            "type": "accounts",
+            "id": "9b3bce6b-e123-4849-b907-9387caebdf9c",
+            "attributes": {
+                "displayName": "🏫 Tuition Fee",
+                "accountType": "SAVER",
+                "balance": {
+                    "currencyCode": "AUD",
+                    "value": "36486.08",
+                    "valueInBaseUnits": 3648608
+                },
+                "createdAt": "2020-02-18T00:00:21+11:00"
+            },
+            "relationships": {
+                "transactions": {
+                    "links": {
+                        "related": "https://api.up.com.au/api/v1/accounts/9b3bce6b-e123-4849-b907-9387caebdf9c/transactions"
+                    }
+                }
+            },
+            "links": {
+                "self": "https://api.up.com.au/api/v1/accounts/9b3bce6b-e123-4849-b907-9387caebdf9c"
+            }
+        },
+        {
+            "type": "accounts",
+            "id": "98964e4b-f61c-45e7-9d6c-7a0c04252907",
+            "attributes": {
+                "displayName": "🏠 Rent",
+                "accountType": "SAVER",
+                "balance": {
+                    "currencyCode": "AUD",
+                    "value": "200.00",
+                    "valueInBaseUnits": 20000
+                },
+                "createdAt": "2020-02-24T14:50:08+11:00"
+            },
+            "relationships": {
+                "transactions": {
+                    "links": {
+                        "related": "https://api.up.com.au/api/v1/accounts/98964e4b-f61c-45e7-9d6c-7a0c04252907/transactions"
+                    }
+                }
+            },
+            "links": {
+                "self": "https://api.up.com.au/api/v1/accounts/98964e4b-f61c-45e7-9d6c-7a0c04252907"
+            }
+        },
+*/
--- Up.Bank.BusinessLogic/Logic/AccountLogic.cs
+++ Up.Bank.BusinessLogic/Logic/AccountLogic.cs
@@ -5,11 +5,17 @@ namespace Up.Bank.BusinessLogic.Logic
     public class AccountLogic
     {
 
+        public Account GetAccountInformation()
+        {
+            var account = new Account();
+            return account;
+        }
+
         public Account GetNewAccount()
         {
-            var account2 = new Account(1999767676, "Chandra", "Dewi", "2A Elsie Street");
+            var account1 = new Account(1999767676, "Chandra", "Dewi", "2A Elsie Street");
 
-            return account2;
+            return account1;
         }
     }
 }
